2fe16fc82288ad5b3b753fffb2f966958df98284
qinlong.wql <qinlong.wql@antgroup.com>
2022-09-13 22:45:18 +0800

    Delete unused imports

--- operator/pkg/controllers/elasticjob_controller.go
+++ operator/pkg/controllers/elasticjob_controller.go
@@ -18,14 +18,11 @@ package controllers
 
 import (
 	"context"
-	"reflect"
 
 	"github.com/go-logr/logr"
-	"github.com/golang/glog"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
