579d36a9051e5ba54c7e4e84e8b3f74fac1dc1ac
Qinlong Wang <WangQL1201@outlook.com>
2022-09-14 11:41:47 +0800

    Fix kubebuilder test

--- operator/api/v1alpha1/groupversion_info.go
+++ operator/api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the elastic v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=elastic.iml.github.io
+// +kubebuilder:object:generate=true
+// +groupName=elastic.iml.github.io
 package v1alpha1
 
 import (
