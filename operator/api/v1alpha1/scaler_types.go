df103a2c1784b13e9eca0835707eebde5a587c6f
Qinlong Wang <WangQL1201@outlook.com>
2022-09-20 12:10:40 +0800

    Define scaler CRD

---
+++ operator/api/v1alpha1/scaler_types.go
@@ -0,0 +1,98 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	commonv1 "github.com/kubeflow/common/pkg/apis/common/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ScalerSpec defines the desired resource state of an ElasticJob
+type ScalerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// ParameterServer specifies the resources of PS for the job.
+	ParameterServer *ReplicaResourceSpec `json:"parameterServer,omitempty"`
+
+	// Worker specifies the resources of workers for the job.
+	Worker *ReplicaResourceSpec `json:"worker,omitempty"`
+
+	// Evaluator specifies the resource of evaluators for the job.
+	Evaluator *ReplicaResourceSpec `json:"evaluator,omitempty"`
+
+	// OwnerJob specifies a job to scale.
+	OwnerJob string `json:"ownerJob,omitempty"`
+}
+
+// ReplicaResourceSpec specifies the number and resources of replica.
+type ReplicaResourceSpec struct {
+	// Replicas is the number of replica
+	Replicas int `json:"replicas,omitempty"`
+
+	// Resource defines the resource of each replica
+	Resource ResourceSpec `json:"resource,omitempty"`
+}
+
+// ResourceSpec specifies the resources of a repalica
+type ResourceSpec struct {
+	// CPU is the requested CPU cores of a replica
+	CPU int32 `json:"cpu,omitempty"`
+	// Memory is the requested memory (MB) of a replica
+	Memory int32 `json:"memory,omitempty"`
+	// GPU is the requested GPU of a replica
+	GPU string `json:"gpu,omitempty"`
+}
+
+// ScalerStatus defines the observed state of ElasticJob
+type ScalerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	commonv1.JobStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+// +resource:path=scaler
+// +kubebuilder:resource:scope=Namespaced
+// +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
+
+// Scaler is the Schema for the scalers API
+type Scaler struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ScalerSpec   `json:"spec,omitempty"`
+	Status ScalerStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ScalerList contains a list of Scale
+type ScalerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Scaler `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Scaler{}, &ScalerList{})
+}
---
+++ operator/pkg/controllers/scaler_controller.go
@@ -0,0 +1,129 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-logr/logr"
+	logger "github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	elasticv1alpha1 "github.com/intelligent-machine-learning/easydl/operator/api/v1alpha1"
+)
+
+const (
+	pollInterval = time.Duration(10)
+)
+
+// ScaleReconciler reconciles an ElasticJob object
+type ScalerReconciler struct {
+	client.Client
+	Scheme   *runtime.Scheme
+	Recorder record.EventRecorder
+	Log      logr.Logger
+}
+
+//+kubebuilder:rbac:groups=elastic.iml.github.io,resources=scaler,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=elastic.iml.github.io,resources=scaler/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=elastic.iml.github.io,resources=scaler/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the ElasticJob object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
+func (r *ScalerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// rlog := r.Log.WithValues("scaler", req.NamespacedName)
+	// Fetch the scale
+	scaler := &elasticv1alpha1.Scaler{}
+	if err := r.Get(context.TODO(), req.NamespacedName, scaler); err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	return r.setScalingOwner(scaler, pollInterval)
+}
+
+func (r *ScalerReconciler) getOwnerJob(scaler *elasticv1alpha1.Scaler) *elasticv1alpha1.ElasticJob {
+	job := &elasticv1alpha1.ElasticJob{}
+	nsn := types.NamespacedName{}
+	nsn.Namespace = scaler.GetNamespace()
+	nsn.Name = scaler.Spec.OwnerJob
+	err := r.Get(context.Background(), nsn, job)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			logger.Warnf("%s not found elasticJob: %v, namespace: %v", scaler.Name, nsn.Name, nsn.Namespace)
+			return nil
+		}
+		return nil
+	}
+	return job
+}
+
+func (r *ScalerReconciler) setScalingOwner(scaler *elasticv1alpha1.Scaler, pollInterval time.Duration) (ctrl.Result, error) {
+	ownerRefs := scaler.GetOwnerReferences()
+	if len(ownerRefs) == 0 {
+		job := r.getOwnerJob(scaler)
+		gvk := elasticv1alpha1.SchemeGroupVersionKind
+		ownerRefs = append(ownerRefs, *metav1.NewControllerRef(job, schema.GroupVersionKind{Group: gvk.Group, Version: gvk.Version, Kind: gvk.Kind}))
+		scaler.SetOwnerReferences(ownerRefs)
+
+		err := r.Status().Update(context.Background(), scaler)
+		if err != nil {
+			logger.Errorf("failed to update scaler status: %s, err: %++v", scaler.Name, err)
+			// Error updating the scaler - requeue the request.
+			return ctrl.Result{RequeueAfter: pollInterval}, nil
+		}
+
+		err = r.Update(context.Background(), scaler)
+		if err != nil {
+			logger.Errorf("failed to update scaler: %s, err: %++v", scaler.Name, err)
+			// Error updating the scaler - requeue the request.
+			return ctrl.Result{RequeueAfter: pollInterval}, nil
+		}
+	}
+	return ctrl.Result{}, nil
+}
+
+func (r *ScalerReconciler) updateElasticJobToScale(job *elasticv1alpha1.ElasticJob) {
+
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ScalerReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&elasticv1alpha1.Scaler{}).
+		Complete(r)
+}
