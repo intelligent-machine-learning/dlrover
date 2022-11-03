dfc35edbe10ddc67725d80f37085e8f1c5a7586c
Qinlong Wang <WangQL1201@outlook.com>
2022-11-03 15:49:18 +0800

    Fix copyright

--- scripts/codestyle/copyright.py
+++ scripts/codestyle/copyright.py
@@ -18,7 +18,7 @@ import subprocess
 import sys
 
 COPYRIGHT = """
-Copyright 2022 The EasyDL Authors. All rights reserved.
+Copyright 2022 The DLRover Authors. All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
