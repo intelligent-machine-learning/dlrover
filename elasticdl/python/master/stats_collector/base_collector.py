7a8b3fc3c9becc6d5e2552752366938289a2b32b
Qinlong Wang <WangQL1201@outlook.com>
2022-11-03 14:59:08 +0800

    Develop the local StatsCollector

---
+++ elasticdl/python/master/stats_collector/base_collector.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod, ABCMeta
+
+
+class StatsCollector(metaclass=ABCMeta):
+    def __init__(self, job_uuid):
+        self._job_uuid = job_uuid
+        self._node_resource_usage = {}
+
+    def collect_node_resource_usage(self, node_name, resource):
+        """Collect the resource usage of the node_name node.
+        Args:
+            node_name: string, the name of node
+            resource: elasticl.python.resource.NodeResource instace,
+                the resource usage of the node.
+        """
+        self._node_resource_usage[node_name] = resource
+
+    @abstractmethod
+    def report_resource_usage(self):
+        pass
---
+++ elasticdl/python/tests/test_local_stats_collector.py
@@ -0,0 +1,22 @@
+import unittest
+
+from elasticdl.python.common.resource import NodeResource
+from elasticdl.python.master.stats_collector.local_collector import (
+    LocalStatsCollector
+)
+
+
+class ALocalStatsCollectorTest(unittest.TestCase):
+    def test_report_resource_usage(self):
+        collector = LocalStatsCollector("123")
+        resource = NodeResource(cpu=1, memory=1024)
+        worker0_name = "worker-0"
+        worker1_name = "worker-1"
+        collector.collect_node_resource_usage(worker0_name, resource)
+        collector.report_resource_usage()
+        collector.collect_node_resource_usage(worker0_name, resource)
+        collector.collect_node_resource_usage(worker1_name, resource)
+        collector.report_resource_usage()
+        self.assertEqual(len(collector._all_node_resources), 2)
+        self.assertEqual(len(collector._all_node_resources[worker0_name]), 2)
+        self.assertEqual(len(collector._all_node_resources[worker1_name]), 1)
