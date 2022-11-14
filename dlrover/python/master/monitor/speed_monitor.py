08c30b9505e7ab1a923ac66f57fa70a7905d6600
Qinlong Wang <WangQL1201@outlook.com>
2022-11-14 19:40:42 +0800

    Format codes

--- dlrover/python/master/monitor/speed_monitor.py
+++ dlrover/python/master/monitor/speed_monitor.py
@@ -1,5 +1,19 @@
+# Copyright 2022 The DLRover Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import time
 from typing import List, Set
+
 from dlrover.python.common.global_context import Context
 from dlrover.python.common.log_utils import default_logger as logger
 
@@ -13,6 +27,7 @@ class GlobalStepRecord(object):
         timestamp: the timestampe to collect the global step.
         worker_num: the number of worker at the timestamp.
     """
+
     def __init__(self, global_step, timestamp, worker_num):
         self.global_step = global_step
         self.timestamp = timestamp
