091edd9ec14edfd51f5d8b68cb45345650cdf3c1
b.sang <samplise@gmail.com>
2022-12-16 15:16:35 -0800

    add cmd to start brain

---
+++ dlrover/go/brain/pkg/config/command_config.go
@@ -0,0 +1,86 @@
+// Copyright 2022 The DLRover Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import "flag"
+
+const (
+	// SpecServerPort is the port of the EasyDL server gRPC service
+	SpecServerPort = "port"
+	// SpecKubeConfigName is the spec name of kube config
+	SpecKubeConfigName = "kubeConfig"
+	// SpecMasterName is the spec name of master name
+	SpecMasterName = "apiServer"
+	// SpecNamespaceName is the spec name of namespace
+	SpecNamespaceName = "namespace"
+	// SpecClusterName is the spec name of cluster
+	SpecClusterName = "cluster"
+	// SpecServiceConfigMapName is the spec name of service config map name
+	SpecServiceConfigMapName = "serviceConfigMapName"
+	// SpecServiceConfigMapKey is the spec name of service config map key
+	SpecServiceConfigMapKey = "serviceConfigMapKey"
+)
+
+// Spec is the struct of configure specifications
+type Spec struct {
+	// Port is the port of the EasyDL server gRPC service
+	Port string
+	// ApiServer is the api server
+	APIServer string
+	// KubeConfig is the kube config
+	KubeConfig string
+	// Cluster is the cluster name
+	Cluster string
+	// Namespace is the namespace
+	Namespace string
+	// ServiceConfigMapName is the service config map name
+	ServiceConfigMapName string
+	// ServiceConfigMapKey is the service config map key
+	ServiceConfigMapKey string
+}
+
+// CommandConfig is the variable of type ConfigSpec
+var CommandConfig = &Spec{}
+
+func init() {
+	flag.StringVar(
+		&CommandConfig.Port,
+		SpecServerPort,
+		":50001",
+		"Port of the EasyDL server gRPC service")
+	flag.StringVar(&CommandConfig.KubeConfig,
+		SpecKubeConfigName,
+		"",
+		"Path to a kubeConfig. Only required if out-of-cluster.")
+	flag.StringVar(&CommandConfig.APIServer,
+		SpecMasterName,
+		"",
+		"The address of the Kubernetes API server. Overrides any value in kubeConfig. Only required if out-of-cluster.")
+	flag.StringVar(&CommandConfig.Namespace,
+		SpecNamespaceName,
+		"",
+		"The namespace name used for k8s ConfigMap & LeaderElection")
+	flag.StringVar(&CommandConfig.Cluster,
+		SpecClusterName,
+		"",
+		"The namespace name used for k8s ConfigMap & LeaderElection")
+	flag.StringVar(&CommandConfig.ServiceConfigMapName,
+		SpecServiceConfigMapName,
+		"",
+		"The name of k8s config map")
+	flag.StringVar(&CommandConfig.ServiceConfigMapKey,
+		SpecServiceConfigMapKey,
+		"",
+		"The key of k8s config map")
+}
