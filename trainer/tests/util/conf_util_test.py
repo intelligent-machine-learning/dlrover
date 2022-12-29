4ffb0d6210e8f85c1e07d544215015c4424968e2
hanxudong <hanxudong.hxd@antgroup.com>
2022-12-29 16:46:31 +0800

    add test case

--- trainer/tests/util/conf_util_test.py
+++ trainer/tests/util/conf_util_test.py
@@ -1,13 +1,31 @@
-import unittest 
+# Copyright 2022 The DLRover Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from trainer.util.conf_util import ConfigurationManagerMeta, ConfigurationManagerInterface, get_conf
+import unittest
+
+from trainer.util.conf_util import (
+    ConfigurationManagerInterface,
+    ConfigurationManagerMeta,
+    get_conf,
+)
+
 
 class NewConfigurationManagerTest(unittest.TestCase):
     def tearDown(self):
         ConfigurationManagerMeta._all_conf_by_name = {}
-        ConfigurationManagerMeta._final_attrs = {}   
-        ConfigurationManagerMeta._override_order = []   
-        ConfigurationManagerMeta._default_priority = 1 << 30  
+        ConfigurationManagerMeta._final_attrs = {}
+        ConfigurationManagerMeta._override_order = []
+        ConfigurationManagerMeta._default_priority = 1 << 30
 
     def test_priority(self):
         class a(ConfigurationManagerInterface):
@@ -132,19 +150,17 @@ class NewConfigurationManagerTest(unittest.TestCase):
             test_set = {"path": "dd", "sls": {"ak": "23", "sk": "ss"}}
 
         final_conf = ConfigurationManagerMeta.merge_configs()
-        self.assertTrue(final_conf.get("a")==22)
+        self.assertTrue(final_conf.get("a") == 22)
         final_conf.clear()
         self.assertEqual(str(final_conf), "{}")
 
-
     def test_get_conf(self):
-
         class TrainConf(object):
-            batch_size = 64 
-            log_steps = 100 
-            save_steps = 1000 
-            save_min_secs = 60  
-            save_max_secs = 60*6
+            batch_size = 64
+            log_steps = 100
+            save_steps = 1000
+            save_min_secs = 60
+            save_max_secs = 60 * 6
 
             params = {
                 "deep_embedding_dim": 8,
@@ -153,13 +169,14 @@ class NewConfigurationManagerTest(unittest.TestCase):
                 "l21": 0.0,
                 "l2": 0.0,
                 "optimizer": "group_adam",
-                "log_steps": 100
+                "log_steps": 100,
             }
-        conf = get_conf(TrainConf)
-        self.assertTrue(conf.get("batch_size")==64)
-        self.assertTrue(conf.get("epoch")==None)
-        self.assertTrue(conf.get("epoch", 1)==1)
 
- 
+        conf = get_conf(TrainConf)
+        self.assertTrue(conf.get("batch_size") == 64)
+        self.assertTrue(conf.get("epoch") is None)
+        self.assertTrue(conf.get("epoch", 1) == 1)
+
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
