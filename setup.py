9724c8ca37219f3cdd5576d4ddeb16820ed689a9
Qinlong Wang <WangQL1201@outlook.com>
2023-01-10 16:28:13 +0800

    Format codes

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
+# Copyright 2023 The DLRover Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import find_packages, setup
 
 with open("dlrover/requirements.txt") as f:
     required_deps = f.read().splitlines()
 
-extras = {}
 
 setup(
     name="dlrover",
@@ -18,7 +30,6 @@ setup(
     author="Ant Group",
     url="https://github.com/intelligent-machine-learning/dlrover",
     install_requires=required_deps,
-    extras_require=extras,
     python_requires=">=3.5",
     packages=find_packages(
         exclude=[
