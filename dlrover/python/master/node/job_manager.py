2ff789447fcdd3f368d335cd21c5e3aac69bb03a
Qinlong Wang <WangQL1201@outlook.com>
2023-02-22 11:19:59 +0800

    Add the memory to relaunch failed worker.

--- dlrover/python/master/node/job_manager.py
+++ dlrover/python/master/node/job_manager.py
@@ -426,7 +426,11 @@ class JobManager(object):
         )
         if should_relaunch:
             if node.exit_reason == NodeExitReason.FATAL_ERROR:
-                should_relaunch = False
+                if node.relaunch_count == 0 and not node.critical:
+                    # Worker may fail by core dump with insufficient memory.
+                    self._job_optimizer.adjust_oom_worker_resource(node)
+                else:
+                    should_relaunch = False
             elif node.exit_reason == NodeExitReason.OOM:
                 mem = node.config_resource.memory
                 if mem > NodeResourceLimit.MAX_MEMORY:
