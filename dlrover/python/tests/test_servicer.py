d758d8c0f50c0298d02bab61a6ed131b707b7009
hanxudong <hanxudong.hxd@antgroup.com>
2023-02-27 11:00:00 +0800

    remove log

--- dlrover/python/tests/test_servicer.py
+++ dlrover/python/tests/test_servicer.py
@@ -158,9 +158,6 @@ class MasterServicerTest(unittest.TestCase):
         self.job_manager._init_nodes()
         for node in self.job_manager._job_nodes[NodeType.PS].values():
             node.status = NodeStatus.RUNNING
-        import pdb
-
-        pdb.set_trace()
         res = self.servicer.query_ps_nodes(request, None)
         self.assertEqual(len(res.ps_nodes), 3)
         self.assertEqual(
