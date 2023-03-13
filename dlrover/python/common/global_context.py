3da411ad0a2b42ca35dc4e843767ae321dd62900
Qinlong Wang <WangQL1201@outlook.com>
2023-03-13 12:29:53 +0800

    Remove unused attributes

--- dlrover/python/common/global_context.py
+++ dlrover/python/common/global_context.py
@@ -97,7 +97,6 @@ class Context(object):
         self.auto_worker_enabled = False
         self.auto_ps_enabled = False
         self.is_tfv1_ps = False
-        self.launch_worker_after_ps_running = False
         self.print_config()
 
     def print_config(self):
