6e7ff6b953982ab834007f32f45cd8a6ff087a1f
hanxudong <hanxdmech@163.com>
2023-03-16 15:52:16 +0800

    fix tf config error

--- dlrover/trainer/tensorflow/failover/tensorflow_failover.py
+++ dlrover/trainer/tensorflow/failover/tensorflow_failover.py
@@ -53,7 +53,12 @@ class TensorflowFailover:
         TF_CONFIG = get_tf_config()
         task_type, task_id = get_tf_config_task_type_and_index()
         self._role = task_type + ":" + str(task_id)
-        self._address = TF_CONFIG["cluster"][task_type][task_id]
+        
+        if len(TF_CONFIG["cluster"][task_type])>1:
+            self._address = TF_CONFIG["cluster"][task_type][task_id]
+        else:
+            self._address = TF_CONFIG["cluster"][task_type][0]
+
         if self._role is None:
             return
         self.task_type, self.task_id = self._role.split(":")
