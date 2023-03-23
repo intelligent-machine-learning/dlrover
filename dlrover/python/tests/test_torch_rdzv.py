6a633c12911408970dc1ca41ed998f5d841b2326
Qinlong Wang <WangQL1201@outlook.com>
2023-03-23 13:08:14 +0800

    Format codes

--- dlrover/python/tests/test_torch_rdzv.py
+++ dlrover/python/tests/test_torch_rdzv.py
@@ -1,8 +1,23 @@
-import unittest
+# Copyright 2023 The DLRover Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import datetime
+import unittest
 
 from dlrover.python.elastic_agent.torch.master_kv_store import MasterKVStore
-from dlrover.python.elastic_agent.torch.rdzv_backend import DlroverRendezvousBackend
+from dlrover.python.elastic_agent.torch.rdzv_backend import (
+    DlroverRendezvousBackend,
+)
 
 
 class MasterKVStoreTest(unittest.TestCase):
@@ -27,14 +42,15 @@ class MasterKVStoreTest(unittest.TestCase):
 class DlroverRendezvousBackendTest(unittest.TestCase):
     def test_set_and_get_state(self):
         rdzv_backend = DlroverRendezvousBackend(
-            "test-rdzv", "torch.elastic.redzv.",
+            "test-rdzv",
+            "torch.elastic.redzv.",
         )
         state_bits = "rdzv-state".encode()
         new_state, token, success = rdzv_backend.set_state(state_bits, 1)
         self.assertEqual(new_state, state_bits)
         self.assertTrue(success)
         new_state, token, success = rdzv_backend.set_state(state_bits, 1)
-        self.assertFalse(success) 
+        self.assertFalse(success)
         new_state, token = rdzv_backend.get_state()
         self.assertEqual(new_state, state_bits)
         self.assertEqual(token, 1)
