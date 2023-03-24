214c4a103a9df9c57cf0cf94a5a6950a20336cca
Qinlong Wang <WangQL1201@outlook.com>
2023-03-24 15:55:06 +0800

    Remove unused class definitions

--- dlrover/python/elastic_agent/torch/rdzv_backend.py
+++ dlrover/python/elastic_agent/torch/rdzv_backend.py
@@ -12,8 +12,7 @@
 # limitations under the License.
 
 import pickle
-from datetime import datetime
-from typing import Dict, Optional, Set, Tuple
+from typing import Optional, Tuple
 
 import grpc
 from torch.distributed import Store
@@ -31,67 +30,6 @@ from dlrover.python.elastic_agent.master_client import (
     MasterClient,
 )
 from dlrover.python.elastic_agent.torch.master_kv_store import MasterKVStore
-
-
-class _NodeDesc:
-    """Describes a node in the rendezvous.
-
-    Attributes:
-        addr:
-            The FQDN of the node or user specified local node address.
-        pid:
-            The id of the process in which the rendezvous handler runs.
-        local_id:
-            A process-wide unique id.
-    """
-
-    addr: str
-    pid: int
-    local_id: int
-
-    def __repr__(self) -> str:
-        return f"{self.addr}_{self.pid}_{self.local_id}"
-
-
-class RendezvousState:
-    """Holds the state of a rendezvous.
-
-    Attributes:
-        round:
-            The current round of the rendezvous.
-        complete:
-            A boolean value indicating whether the current round of the
-            rendezvous is complete.
-        deadline:
-            The time at which the current round of the rendezvous will be
-            considered complete if it is still waiting for nodes to join.
-        closed:
-            A boolean value indicating whether the rendezvous is closed.
-        participants:
-            A dictionary of the participants and their corresponding ranks.
-        wait_list:
-            A set of nodes that are waiting to participate in the next round of
-            the rendezvous.
-        last_heartbeats:
-            A dictionary containing each node's last heartbeat time.
-    """
-
-    round: int
-    complete: bool
-    deadline: Optional[datetime]
-    closed: bool
-    participants: Dict[_NodeDesc, int]
-    wait_list: Set[_NodeDesc]
-    last_heartbeats: Dict[_NodeDesc, datetime]
-
-    def __init__(self) -> None:
-        self.round = 0
-        self.complete = False
-        self.deadline = None
-        self.closed = False
-        self.participants = {}
-        self.wait_list = set()
-        self.last_heartbeats = {}
 
 
 class DlroverRendezvousBackend(RendezvousBackend):
