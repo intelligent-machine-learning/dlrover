08a68dcc53c9b8a511ee746cc40e3c070c4a45ca
jian.sha <jian.sha@antgroup.com>
2023-05-23 18:32:23 +0800

    fix ci.

--- atorch/atorch/__init__.py
+++ atorch/atorch/__init__.py
@@ -1,8 +1,5 @@
 import os
 
 # from .distributed.distributed import coworker_size, init_distributed, local_rank, rank, reset_distributed, world_size
-from .utils.metrics_reporter import report_import
-
-report_import()
 
 os.environ["PIPPY_PIN_DEVICE"] = "0"
