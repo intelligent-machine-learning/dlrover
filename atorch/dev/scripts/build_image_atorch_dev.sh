34a59a5397d6bc856cc98290397ce5533a85e9e8
jian.sha <jian.sha@antgroup.com>
2023-05-23 20:52:24 +0800

    fix ci.

--- atorch/dev/scripts/build_image_atorch_dev.sh
+++ atorch/dev/scripts/build_image_atorch_dev.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 tag=$1
+dockerfile=$2
+
 if [ -z $tag ]
 then
    read -p "image tag is NULL, please input: " tag
 fi
-sudo docker build -f tools/docker/Dockerfile-dev --net host -t "reg.docker.alibaba-inc.com/atorch/atorch-dev:$tag" ./
+
+if [ -z $dockerfile ]
+then
+   read -p "dockerfile path is empty, please input: " dockerfile
+fi
+
+sudo docker build -f $dockerfile --net host -t "reg.docker.alibaba-inc.com/atorch/atorch-dev:$tag" ./
