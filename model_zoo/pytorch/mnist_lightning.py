de8fc9349a6ba2d53874237084bb68740db6ae47
Qinlong Wang <WangQL1201@outlook.com>
2023-06-05 11:01:23 +0800

    Format codes

--- model_zoo/pytorch/mnist_lightning.py
+++ model_zoo/pytorch/mnist_lightning.py
@@ -1,17 +1,37 @@
+# Copyright 2023 The DLRover Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
-import torch
-from torch import nn
+
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader
-from torch.nn import functional as F
-from torchvision import transforms
+import torch
 import torchvision
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
 from dlrover.trainer.torch.elastic_sampler import ElasticDistributedSampler
 
 
 class UpdateDataStepCallback(pl.Callback):
     def on_train_batch_end(
-        self, trainer, pl_module, outputs, batch, batch_idx,
+        self,
+        trainer,
+        pl_module,
+        outputs,
+        batch,
+        batch_idx,
     ) -> None:
         trainer.datamodule.global_step = trainer.global_step
 
@@ -51,14 +71,14 @@ class LightningMNISTClassifier(pl.LightningModule):
         x, y = train_batch
         logits = self.forward(x)
         loss = self.cross_entropy_loss(logits, y)
-        self.log('train_loss', loss, on_step=True, on_epoch=True)
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
         x, y = val_batch
         logits = self.forward(x)
         loss = self.cross_entropy_loss(logits, y)
-        self.log('val_loss', loss, on_step=True, on_epoch=True)
+        self.log("val_loss", loss, on_step=True, on_epoch=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
@@ -66,7 +86,6 @@ class LightningMNISTClassifier(pl.LightningModule):
 
 
 class MNISTDataModule(pl.LightningDataModule):
-
     def __init__(self, train_dir, test_dir, batch_size, shuffle):
         self.train_dir = train_dir
         self.test_dir = test_dir
@@ -77,17 +96,18 @@ class MNISTDataModule(pl.LightningDataModule):
     def setup(self, stage):
         # transforms for images
         transform = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Normalize((0.1307,), (0.3081,))]
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
         )
 
         # prepare transforms standard to MNIST
         self.mnist_train = torchvision.datasets.ImageFolder(
-            self.train_dir, transform=transform,
+            self.train_dir,
+            transform=transform,
         )
 
         self.mnist_test = torchvision.datasets.ImageFolder(
-            self.test_dir, transform=transform,
+            self.test_dir,
+            transform=transform,
         )
         self.train_sampler = ElasticDistributedSampler(
             self.mnist_train,
