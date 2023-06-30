d481dd3b4fae77176b6a325683fe7c48f5b0e38a
Qinlong Wang <WangQL1201@outlook.com>
2023-06-30 11:24:56 +0800

    Rendezvous manager to help the node to check network. (#465)
    * Implement rdzv manager for network check
    
    * Add docstring
    
    * Remove unused codes
    
    * Remove annotation codes
    
    * Merge test cases
    
    * Merge codes
    
    * Format codes

--- dlrover/python/master/main.py
+++ dlrover/python/master/main.py
@@ -38,7 +38,7 @@ def run(args):
     job_args.initilize()
     logger.info("Job args : %s", job_args.toJSON())
     _dlrover_context.config_master_port(port=args.port)
-    _dlrover_context.relaunch_error = args.relaunch_error
+    _dlrover_context.relaunch_error = args.relaunch_error or True
     update_context(job_args)
     master = Master(_dlrover_context.master_port, job_args)
     master.prepare()
