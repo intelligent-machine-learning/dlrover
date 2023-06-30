b6ea41927d909aa69ba98fe53d040e4508241bc2
Qinlong Wang <WangQL1201@outlook.com>
2023-06-30 16:54:32 +0800

    Cast join timeout to int

--- dlrover/python/elastic_agent/torch/training.py
+++ dlrover/python/elastic_agent/torch/training.py
@@ -73,7 +73,7 @@ class MasterRendezvousHandler(RendezvousHandler):
         self.join_timeout = int(rdzv_params.get("join_timeout", 600))
         self._client = GlobalMasterClient.MASTER_CLIENT
         self._store = MasterKVStore(self._name, timedelta(seconds=60))
-        lastcall_timeout = rdzv_params.get("lastcall_timeout", 60)
+        lastcall_timeout = int(rdzv_params.get("lastcall_timeout", 60))
         self._client.report_rdzv_params(
             rdzv_params.min_nodes,
             rdzv_params.max_nodes,
