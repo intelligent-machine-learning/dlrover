7d601bd1a38ce87970f5ec7075074847d774b44d
Qinlong Wang <WangQL1201@outlook.com>
2023-07-27 19:48:35 +0800

    Log the rdzv params from rank 0 (#539)

--- dlrover/python/master/scaler/pod_scaler.py
+++ dlrover/python/master/scaler/pod_scaler.py
@@ -125,7 +125,7 @@ class PodScaler(Scaler):
     def scale(self, plan: ScalePlan):
         """Scale in/out Pods by a ScalePlan."""
         self._plan = plan
-        job_pods = self._wait_list_pods()
+        job_pods = self._list_job_pods()
         logger.info("Scale the job by plan %s", plan.toJSON())
 
         with self._lock:
@@ -152,17 +152,6 @@ class PodScaler(Scaler):
                 if not removed:
                     self._k8s_client.delete_pod(node.name)
             self._update_job_pods(job_pods)
-
-    def _wait_list_pods(self, timeout=1800):
-        start = time.time()
-        while True:
-            pods = self._list_job_pods()
-            if pods:
-                return pods
-            if time.time() - start < timeout:
-                time.sleep(60)
-            else:
-                raise TimeoutError(f"Timeout {timeout} to list Pods.")
 
     def _update_job_pods(self, job_pods: Dict[str, List[Node]]):
         for type in [
@@ -196,8 +185,7 @@ class PodScaler(Scaler):
         return False
 
     def _list_job_pods(self):
-        job_selector = ElasticJobLabel.JOB_KEY + "=" + self._job_name
-        pod_list = self._k8s_client.list_namespaced_pod(job_selector)
+        pod_list = self._wait_list_pods()
         job_pods: Dict[str, List[Node]] = {}
         if not pod_list:
             return job_pods
@@ -221,6 +209,18 @@ class PodScaler(Scaler):
             )
             job_pods[pod_type].append(node)
         return job_pods
+
+    def _wait_list_pods(self, timeout=1800):
+        job_selector = ElasticJobLabel.JOB_KEY + "=" + self._job_name
+        start = time.time()
+        while True:
+            pod_list = self._k8s_client.list_namespaced_pod(job_selector)
+            if pod_list:
+                return pod_list
+            if time.time() - start < timeout:
+                time.sleep(60)
+            else:
+                raise TimeoutError(f"Timeout {timeout} to list Pods.")
 
     def _get_pod_resource(self, pod):
         resources = pod.spec.containers[0].resources
