8b6a1b18546f480c1935f0dbc0c259d7f4605dd1
Qinlong Wang <WangQL1201@outlook.com>
2023-08-04 15:20:02 +0800

    Skip the empty scale plan.

--- dlrover/python/master/scaler/pod_scaler.py
+++ dlrover/python/master/scaler/pod_scaler.py
@@ -126,6 +126,8 @@ class PodScaler(Scaler):
         """Scale in/out Pods by a ScalePlan."""
         self._plan = plan
         job_pods = self._list_job_pods()
+        if plan.empty():
+            return
         logger.info("Scale the job by plan %s", plan.toJSON())
 
         with self._lock:
