a7b8ba68b9ac706f6049f346fb89a6ca5589ddc4
Qinlong Wang <WangQL1201@outlook.com>
2023-08-10 13:51:14 +0800

    feat: Launch the local master in a subprocess if standalone. (#598)
    * feat: Launch the local master in a subprocess if standalone.
    
    * Format codes.
    
    * Use torch launch_agent if the DLRover job master is no available.
    
    * test: add test cases of elastic_run
    
    * refactor: format codes

--- dlrover/trainer/torch/elastic_run.py
+++ dlrover/trainer/torch/elastic_run.py
@@ -11,15 +11,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import sys
+import telnetlib
+import tempfile
+import time
 import uuid
 from typing import Callable, Union
 
 from torch.distributed.argparse_util import check_env, env
+from torch.distributed.elastic.multiprocessing.api import SubprocessHandler
 from torch.distributed.elastic.multiprocessing.errors import record
 from torch.distributed.launcher.api import LaunchConfig
+from torch.distributed.launcher.api import launch_agent as torch_launch_agent
 from torch.distributed.run import config_from_args, get_args_parser
 
+from dlrover.python.common.grpc import find_free_port
 from dlrover.python.common.log import default_logger as logger
+from dlrover.python.elastic_agent.master_client import (
+    GlobalMasterClient,
+    build_master_client,
+)
 from dlrover.python.elastic_agent.torch.training import launch_agent
 
 
@@ -76,16 +88,76 @@ class elastic_launch:
         self,
         config: LaunchConfig,
         entrypoint: Union[Callable, str, None],
+        use_dlrover_launch: bool,
     ):
         self._config = config
         self._entrypoint = entrypoint
+        self._use_dlrover_launch = use_dlrover_launch
 
     def __call__(self, *args):
-        return launch_agent(self._config, self._entrypoint, list(args))
+        if self._use_dlrover_launch:
+            return launch_agent(self._config, self._entrypoint, list(args))
+        else:
+            return torch_launch_agent(
+                self._config, self._entrypoint, list(args)
+            )
+
+
+def _launch_dlrover_local_master():
+    """Launch a subprocess to run the DLrover master."""
+    cmd = os.getenv("PYTHON_EXEC", sys.executable)
+    host = "127.0.0.1"
+    port = find_free_port()
+    log_dir = tempfile.mkdtemp(prefix="dlrover_master_")
+    job_name = log_dir.split("_")[-1]
+    stdout = os.path.join(log_dir, "stdout.log")
+    stderr = os.path.join(log_dir, "stderror.log")
+    logger.info(f"The master log file:\n stdout: {stdout} \n stderr: {stderr}")
+    args = (
+        "-u",
+        "-m",
+        "dlrover.python.master.main",
+        "--port",
+        f"{port}",
+        "--job_name",
+        f"standalone-{job_name}",
+        "--platform",
+        "local",
+    )
+    handler = SubprocessHandler(cmd, args, {}, stdout, stderr)
+    dlrover_master_addr = f"{host}:{port}"
+    return handler, dlrover_master_addr
+
+
+def _check_dlrover_master_available(addr, timeout=60):
+    """Check whether the master grpc servicer is available."""
+    host = addr.split(":")[0]
+    port = int(addr.split(":")[1])
+    start_time = time.time()
+    while True:
+        try:
+            telnetlib.Telnet(host=host, port=port, timeout=1)
+            logger.info("DLRover job master starts!")
+            return True
+        except ConnectionRefusedError:
+            time.sleep(1)
+        if time.time() - start_time > 60:
+            return False
 
 
 def run(args):
+    master_handler = None
+    dmaster_addr = os.getenv("DLROVER_MASTER_ADDR", "")
+    use_dlrover_launch = False
     if args.standalone:
+        master_handler, dmaster_addr = _launch_dlrover_local_master()
+    if _check_dlrover_master_available():
+        GlobalMasterClient.MASTER_CLIENT = build_master_client(dmaster_addr)
+        use_dlrover_launch = True
+    else:
+        use_dlrover_launch = False
+
+    if args.standalone and not use_dlrover_launch:
         args.rdzv_backend = "c10d"
         args.rdzv_endpoint = "localhost:29400"
         args.rdzv_id = str(uuid.uuid4())
@@ -106,9 +178,11 @@ def run(args):
     if hasattr(args, "node_unit"):
         config.rdzv_configs["node_unit"] = args.node_unit
     elastic_launch(
-        config=config,
-        entrypoint=cmd,
+        config=config, entrypoint=cmd, use_dlrover_launch=use_dlrover_launch
     )(*cmd_args)
+
+    if master_handler:
+        master_handler.close()
 
 
 @record
