c64346ac0d0f3d623b1ce54efbda667b2dc00d2f
Tingfeng Lan <tafflann@outlook.com>
2023-09-21 11:54:51 +0000

    Fix pynvml exception handling.

--- dlrover/python/elastic_agent/monitor/resource.py
+++ dlrover/python/elastic_agent/monitor/resource.py
@@ -129,7 +129,7 @@ class ResourceMonitor(object):
         try:
             pynvml.nvmlInit()
             self._gpu_enabled = True
-        except pynvml.NVML_ERROR_LIBRARY_NOT_FOUND:
+        except pynvml.NVMLError_LibraryNotFound:
             logger.warn(
                 "NVIDIA NVML library not found. "
                 "GPU monitoring features will be disabled."
