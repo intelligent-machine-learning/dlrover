579218014194996eaae01e55bfafc4b8256ec688
Qinlong Wang <WangQL1201@outlook.com>
2023-10-09 15:59:21 +0800

    The method to suggest stop. (#745)

--- dlrover/python/master/node/job_auto_scaler.py
+++ dlrover/python/master/node/job_auto_scaler.py
@@ -73,6 +73,12 @@ def new_job_auto_scaler(
 class JobAutoScaler(metaclass=ABCMeta):
     """JobAutoScaler automatically scale up/down nodes of job."""
 
+    def __init__(self):
+        self._suggested_stop = False
+
+    def suggested_stop(self):
+        return self._suggested_stop
+
     @abstractmethod
     def start_auto_scaling(self):
         """Start auto-scaling nodes of a job"""
@@ -102,6 +108,7 @@ class PSTrainingAutoScaler(JobAutoScaler):
         worker_manager: WorkerManager,
         node_scaler: Scaler,
     ) -> None:
+        super().__init__()
         self._job_resource = job_resource
         self._job_optimizer = job_optimizer
         self._speed_monitor = speed_monitor
@@ -256,6 +263,7 @@ class AllreduceTrainingAutoScaler(JobAutoScaler):
         worker_manager: WorkerManager,
         node_scaler: Scaler,
     ) -> None:
+        super().__init__()
         self._job_resource = job_resource
         self._job_optimizer = job_optimizer
         self._speed_monitor = speed_monitor
