5a295deed9126bf4be17d8c5c8933dd1d40043e9
skydoorkai <htsantaclara@163.com>
2023-12-20 15:47:47 +0800

    remove file

--- atorch/atorch/ops/git_version_info_installed.py
+++
@@ -1,6 +0,0 @@
-version = '1.1.1.dev2+dca06275'
-git_hash = 'dca06275'
-git_branch = 'dlrover_testing'
-installed_ops = {'quantization_optimizer': False, 'quantizer': False}
-compatible_ops = {'quantization_optimizer': True, 'quantizer': True, 'atorch_not_implemented': False}
-torch_info = {'version': '1.12', 'bf16_support': False, 'cuda_version': '11.3', 'nccl_version': '2.10'}
