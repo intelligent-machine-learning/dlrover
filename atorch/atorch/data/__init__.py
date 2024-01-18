23862192cda8070163e4e404d4b34dbddd2618b9
skydoorkai <htsantaclara@163.com>
2024-01-18 19:49:26 +0800

    handle protobuf version mismatch

--- atorch/atorch/data/__init__.py
+++ atorch/atorch/data/__init__.py
@@ -1,6 +1,12 @@
 from .coworker_dataset import build_coworker_dataloader
 from .data_utils import expand_batch_dim, get_sample_batch
-from .elastic_dataloader import build_coworker_dataloader_with_elasticdl, get_elastic_dataloader
+
+try:
+    from .elastic_dataloader import build_coworker_dataloader_with_elasticdl, get_elastic_dataloader
+except TypeError:
+    print("protobuf version mismatch. elastic_dataloader cannot be used")
+    build_coworker_dataloader_with_elasticdl = None
+    get_elastic_dataloader = None
 from .preloader import GpuPreLoader, data_to_device
 from .shm_context import ShmData, create_coworker_shm_context
 from .shm_dataloader import ShmDataloader, create_shm_dataloader
