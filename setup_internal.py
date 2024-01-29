e6696fcefd3bad563f2a877a20655ec6c25ff041
qinlong.wql <qinlong.wql@antgroup.com>
2024-01-29 15:00:00 +0800

    Update protobuf.

--- setup_internal.py
+++ setup_internal.py
@@ -14,8 +14,8 @@
 from setuptools import find_packages, setup
 
 install_requires = [
-    "grpcio-tools==1.34.1",
-    "protobuf>=3.15.3,<4.0dev",
+    "grpcio-tools>=1.58.0",
+    "protobuf>=3.20.3,<4.0dev",
     "psutil",
     "pynvml",
     "urllib3<1.27,>=1.21.1",
