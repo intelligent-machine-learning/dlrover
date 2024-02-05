c9d02ef5013ec14a4dbfd403e3531ce9513fedc2
Qinlong Wang <WangQL1201@outlook.com>
2024-02-05 14:33:27 +0800

    Restore apis.

--- dlrover/python/common/singleton.py
+++ dlrover/python/common/singleton.py
@@ -14,6 +14,19 @@
 import threading
 
 
+def singleton(cls):
+    _instance = {}
+    _instace_lock = threading.Lock()
+
+    def _singleton(*args, **kwargs):
+        with _instace_lock:
+            if cls not in _instance:
+                _instance[cls] = cls(*args, **kwargs)
+        return _instance[cls]
+
+    return _singleton
+
+
 class Singleton(object):
     _instance_lock = threading.Lock()
     _instance = None
