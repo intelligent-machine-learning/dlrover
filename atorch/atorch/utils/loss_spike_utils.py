5e395bae5dcf9566099fa3bfff47cc088ccbbc34
hktk <liyingda.lyd@antgroup.com>
2024-04-03 10:22:53 +0800

    优化注释

--- atorch/atorch/utils/loss_spike_utils.py
+++ atorch/atorch/utils/loss_spike_utils.py
@@ -25,13 +25,14 @@ class LossSpikeBase:
         init params
         Args:
             loss_spike_save_dir: str, The directory where loss spike files are stored
-            sample_data_paths: 样本存储的路径信息, 可以是用户自定义的结构，如名称+路径的tuple list:
+            sample_data_paths: The path information stored in the sample can be a user-defined structure,
+                such as name + path, tuple list:
                 [("wikipedia", "corpus/base"), ("zhihu", "/dataset/fd5061f6/data/tokenize_data/zhihu.lazy")]
-            each_sample_len: int, 单个样本的长度
-            min_iter: int, 记录loss所需的最小迭代轮数
-            min_loss: float, 记录loss所需的最小loss阈值
-            loss_info_splitter: str, 存储loss尖刺信息时所用的分隔符, 默认值='\t'
-            loss_sample_str_splitter: str, 一批loss或者sample信息组成的str所用的分隔符, 默认值=','
+            each_sample_len: int, The length of a single sample
+            min_iter: int, Record the minimum iterations required for loss
+            min_loss: float, Record the minimum loss threshold required for loss
+            loss_info_splitter: str, Delimiter used to store loss spike information, default ='\t'
+            loss_sample_str_splitter: str, Delimiter used by str for a batch of loss or sample information, default =','
         """
         self.loss_spike_save_dir = loss_spike_save_dir
         self.sample_data_paths = sample_data_paths
@@ -55,21 +56,21 @@ class LossSpikeBase:
 class TokenLossSpike(LossSpikeBase):
     def save_loss(self, file_name, cur_loss, cur_iter, losses_str, sample_infos_str):
         """
-        存储尖刺loss及对应的一些信息
+        Store spike loss and corresponding information
         Args:
             file_name: str, loss spike file name
             cur_loss: float, current loss value
             cur_iter: int, current iteration
-            losses_str: A string of loss concatenated with splitter, 如:
+            losses_str: A string of loss concatenated with splitter, eg:
                 "2.31,2.30,10.98,1.56"
-            sample_infos_str: A string of sample id info concatenated with splitter, 如:
+            sample_infos_str: A string of sample id info concatenated with splitter, eg:
                 "20-17-1385697-14158189-936633,20-17-1385697-14158189-936633"
 
         """
         file_path = os.path.join(self.loss_spike_save_dir, file_name)
         if cur_loss > self.min_loss and cur_iter > self.min_iter:
             logger.info(f"save loss={cur_loss}, iter={cur_iter}")
-            # 存储结构需要定义
+            # define structure
             cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             info = self.loss_info_splitter.join([cur_time, str(cur_iter), str(cur_loss), losses_str, sample_infos_str])
             info = info + "\n"
@@ -78,12 +79,12 @@ class TokenLossSpike(LossSpikeBase):
 
     def decode_loss_spike(self, result_file_path, tokenizer, min_iter=None, min_loss=None):
         """
-        根据文件中的尖刺loss等信息，解析出对应的样本到文件
+        According to the information such as spike loss in the file, the corresponding sample is parsed to the file
         Args:
-            result_file_path: str, 存储解码后的样本内容的文件地址
-            tokenizer: instance, 样本文件中的存储的样本, 需要时的解码器
-            min_iter: int, 解码loss、sample所需的最小迭代轮数, 不传的话，使用存储时的初始化的阈值
-            min_loss: float, 解码loss、sample所需的最小loss阈值, 不传的话，使用存储时的初始化的阈值
+            result_file_path: str, The address of the file that stores the contents of the decoded sample
+            tokenizer: instance,
+            min_iter: int, minimum iterations required for decode loss、sample
+            min_loss: float,  minimum loss required for decode loss、sample
 
         Returns:
 
@@ -94,13 +95,13 @@ class TokenLossSpike(LossSpikeBase):
             min_loss = self.min_loss
 
         with open(result_file_path, "w") as fw:
-            #  遍历整个目录
+            # Traverse the entire directory
             for loss_spike_file in os.listdir(self.loss_spike_save_dir):
                 file_path = os.path.join(self.loss_spike_save_dir, loss_spike_file)
                 with open(file_path) as fr:
                     for line in fr:
                         # process file content by line
-                        # 内容结构: f"{ctime}\t{iteration}\t{loss}\t{loss_str}\t{sample_ids_str}\n"
+                        # structure: f"{ctime}\t{iteration}\t{loss}\t{loss_str}\t{sample_ids_str}\n"
                         fcontent = line.strip().split(self.loss_info_splitter)
                         cur_iter = int(fcontent[1])
                         cur_loss = float(fcontent[2])
@@ -124,7 +125,7 @@ class TokenLossSpike(LossSpikeBase):
             logger.warn("batch loss length != batch sample length")
             return None, None, None
 
-        losses = np.array(losses)  # 解析loss es
+        losses = np.array(losses)
         idx = losses.argmax(-1)
         max_loss = losses[idx]
         sample_with_max_loss = sample_infos[idx]
@@ -136,12 +137,13 @@ class TokenLossSpike(LossSpikeBase):
         return ds, data, max_loss
 
     def fetch(self, each_sample_info):
-        # 这里是比较定制化的，不同应用场景可以构建不同的子类
+        # Here is more customized, different application scenarios can build different subclasses
         # 20-17-1385697-14158189-936633
         # {scatter_id}-{dsid}-{idx}-{raw_id}-{sample_id}
-        scatter_id, dsid, _, _, sample_id = each_sample_info.split("-")  # 这个结构是在初始化定义好的
+        scatter_id, dsid, _, _, sample_id = each_sample_info.split("-")  # structure is defined during initialization
 
-        ds_info = self.sample_data_paths[int(dsid)]  # 用列表的索引, 0是train data，1是对应的样本PATH
+        # Using the index of the list, 0 is the train data and 1 is the corresponding sample PATH
+        ds_info = self.sample_data_paths[int(dsid)]
 
         datapath = f"{ds_info[1]}.scatter/{scatter_id}.lazy/text"
 
@@ -150,6 +152,6 @@ class TokenLossSpike(LossSpikeBase):
             return None, None
         flen = self.get_data_file_len(datapath, np.dtype(np.int32))
         sample_cnt = flen // self.each_sample_len
-        f = np.memmap(datapath, dtype=np.int32, shape=(sample_cnt, self.each_sample_len))  # 磁盘写内存
+        f = np.memmap(datapath, dtype=np.int32, shape=(sample_cnt, self.each_sample_len))  # Disk to memory
         data = f[int(sample_id)]
         return ds_info[0], data
