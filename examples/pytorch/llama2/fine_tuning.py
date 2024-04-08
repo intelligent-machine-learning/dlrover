9a4e8df5d3dcda27395223c65c819baad634a3e0
cailun01 <cailun.cl@antgroup.com>
2024-04-08 17:54:26 +0800

    fix pre-commit F811, E501

--- examples/pytorch/llama2/fine_tuning.py
+++ examples/pytorch/llama2/fine_tuning.py
@@ -28,20 +28,21 @@ try:
 except (ImportError, ModuleNotFoundError):
 
     def is_torch_npu_available():
-        "Checks if `torch_npu` is installed and potentially if a NPU is in the environment"
+        "Checks if `torch_npu` is installed and potentially "
+        "if a NPU is in the environment"
         import importlib
 
         if importlib.util.find_spec("torch_npu") is None:
             return False
 
         import torch
-        import torch_npu  # noqa: F401
+        import torch_npu  # noqa: F401,F811
 
         return hasattr(torch, "npu") and torch.npu.is_available()
 
 
 if is_torch_npu_available():
-    import torch_npu  # noqa: F401
+    import torch_npu  # noqa: F401,F811
     from torch_npu.contrib import transfer_to_npu  # noqa: F401
 
 
