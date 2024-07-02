e34f2c137e6e5dffe246497e34f83229dd9eb2ab
BO SANG <b.sang@antgroup.com>
2024-07-01 20:12:30 -0700

    add unit test

--- dlrover/python/tests/test_worker_diagnosis.py
+++ dlrover/python/tests/test_worker_diagnosis.py
@@ -28,13 +28,4 @@ class WorkerDiagnosisTest(unittest.TestCase):
         collector = CudaLogCollector(path)
         self.assertTrue(collector.to_collect_data())
 
-        # path = "data/cuda_logs"
-        # collector = CudaLogCollector(path)
-        # collector.collect_data()
-        # # main_traces = log.get_main_traces()
-        # # for rank, trace in main_traces.items():
-        # #     print(f"{rank}: {trace}\n")
-        # complete_traces = collector.get_all_traces()
-        # for trace in complete_traces:
-        #     print(f"{trace}\n")
-        #     print("-----------------------\n")
+
