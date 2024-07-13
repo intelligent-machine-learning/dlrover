f73052021f62967c65b8658c6ddaceeb45e4099f
chentianyi.cty <robert37@sina.com>
2024-07-13 13:41:46 +0800

    lint

--- dlrover/python/master/node/job_manager.py
+++ dlrover/python/master/node/job_manager.py
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import threading
 from abc import ABCMeta, abstractclassmethod
 from typing import Dict
 
