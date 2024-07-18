cb795417d781017eaa7e810ad7be9be2f0e83d65
chentianyi.cty <chentianyi.cty@antgroup.com>
2024-07-18 11:58:08 +0800

    update version 0.4.7

--- setup_internal.py
+++ setup_internal.py
@@ -32,7 +32,7 @@ extra_require = {
 
 setup(
     name="dlrover",
-    version="0.4.6",
+    version="0.4.7",
     description="An Automatic Distributed Deep Learning Framework",
     long_description="DLRover helps model developers focus on model algorithm"
     " itself, without taking care of any engineering stuff,"
