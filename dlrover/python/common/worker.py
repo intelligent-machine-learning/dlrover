93b01458a228e4e1922908b98f0555d3d2dfc1e4
bsang <b.sang@antgroup.com>
2024-08-14 20:46:40 +0800

    add test case

--- dlrover/python/common/worker.py
+++ dlrover/python/common/worker.py
@@ -11,8 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import copy
-
 from torch.distributed.elastic.agent.server.api import RunResult, WorkerSpec
 
 
@@ -24,7 +22,15 @@ class WorkerContext:
         restart_count: int,
         run_result: RunResult,
     ):
-        self.worker_spec: WorkerSpec = copy.deepcopy(worker_spec)
+        self._worker_spec: WorkerSpec = worker_spec
         self.remaining_failovers = remaining_failovers
         self.restart_count = restart_count
-        self.run_result = copy.deepcopy(run_result)
+        self._run_result = run_result
+
+    @property
+    def worker_spec(self):
+        return self._worker_spec
+
+    @property
+    def run_result(self):
+        return self._run_result
