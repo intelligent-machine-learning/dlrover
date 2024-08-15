3cc78f4dab5a0494d75442122c81ed9bee1e6fc1
Tianyi Chen <chentianyi.cty@antfin.com>
2024-08-15 19:11:31 +0800

    Remove error code 128 from 'hardware-error' (#1237)
    * kick out error code 128 from 'hard-ware error'
    
    * optimize logging

--- dlrover/python/common/constants.py
+++ dlrover/python/common/constants.py
@@ -131,7 +131,7 @@ class ExitCode(object):
     GPU_DRIVER_ERROR = 201
     GPU_POD_RESIDUE = 202
     GPU_INFOROM_CORRUPTED = 14
-    UNKNOWN_DEVICE = 128
+    CONTAINER_FAILED_OR_UNKNOWN_DEVICE = 128
 
 
 class NodeResourceLimit(object):
