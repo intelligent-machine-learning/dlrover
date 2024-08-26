f8149015cdc6b695d1514dff297f8e92b3e8ba77
chentianyi.cty <robert37@sina.com>
2024-08-26 17:00:19 +0800

    lint

--- dlrover/python/tests/test_elastic_training_agent.py
+++ dlrover/python/tests/test_elastic_training_agent.py
@@ -517,7 +517,6 @@ class NodeCheckElasticAgentTest(unittest.TestCase):
         self.assertEqual(agent._check_round, 2)
 
     def test_run_agent(self):
-        os.environ["POD_IP"] = "127.0.0.1"
         config = ElasticLaunchConfig(4, 4, 8)
         agent = _create_check_agent(
             config=config,
