0587a12d190a0f7cedc56104fe6d783c729a69eb
chentianyi.cty <robert37@sina.com>
2024-09-10 17:50:20 +0800

    optimize

--- dlrover/python/diagnosis/inferencechain/inference_chain.py
+++ dlrover/python/diagnosis/inferencechain/inference_chain.py
@@ -43,6 +43,8 @@ class InferenceChain:
             for inference in inferences:
                 try:
                     operator = self.get_operator(inference)
+                    if not operator:
+                        continue
                     infs = operator.infer(inferences)
                     if len(infs) > 0:
                         has_new_inference = True
@@ -63,4 +65,4 @@ class InferenceChain:
         for operator in self.operators:
             if operator.is_compatible(inference):
                 return operator
-        raise TypeError(f"no compatible operator for {inference}")
+        return None  # type: ignore
