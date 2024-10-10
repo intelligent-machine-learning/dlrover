3b351e8074069da46ec3352cf20982519eaa0913
chentianyi.cty <robert37@sina.com>
2024-10-10 15:28:48 +0800

    fix diagnosis data rpc

--- dlrover/python/tests/test_servicer.py
+++ dlrover/python/tests/test_servicer.py
@@ -18,7 +18,7 @@ from unittest import mock
 import ray
 
 from dlrover.proto import elastic_training_pb2
-from dlrover.python.common import grpc
+from dlrover.python.common import env_utils, grpc
 from dlrover.python.common.constants import (
     NodeStatus,
     NodeType,
@@ -26,6 +26,7 @@ from dlrover.python.common.constants import (
     RendezvousName,
 )
 from dlrover.python.common.grpc import GPUStats
+from dlrover.python.diagnosis.common.diagnosis_data import WorkerTrainingMetric
 from dlrover.python.master.diagnosis.diagnosis import DiagnosisManager
 from dlrover.python.master.elastic_training.elastic_ps import ElasticPsService
 from dlrover.python.master.elastic_training.rdzv_manager import (
@@ -403,6 +404,22 @@ class MasterServicerTest(unittest.TestCase):
         self.assertFalse(success)
         success = self.servicer._sync_checkpoint(NodeType.WORKER, 1, message)
         self.assertTrue(success)
+
+    def test_report_worker_diagnosis_data(self):
+        test = WorkerTrainingMetric(
+            data_content="test123",
+            node_id=env_utils.get_node_id(),
+            node_type=env_utils.get_node_type(),
+            node_rank=env_utils.get_node_rank(),
+            is_final_result=True,
+        )
+
+        request = grpc.DiagnosisReportData(
+            test.__class__.__name__,
+            test.to_json(),
+            test.node_rank,
+        )
+        self.assertTrue(self.servicer._report_worker_diagnosis_data(request))
 
 
 class MasterServicerForRayTest(unittest.TestCase):
