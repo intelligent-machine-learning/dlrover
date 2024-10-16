49db0f48f3ee69cf095a6f7d17550a2dc702230f
bsang <b.sang@antgroup.com>
2024-10-16 09:54:56 +0800

    remove log

--- dlrover/python/master/node/training_node.py
+++ dlrover/python/master/node/training_node.py
@@ -470,7 +470,6 @@ class TrainingNodeConfig:
                 )
 
     def get_elastic_run_configs(self) -> Dict[str, str]:
-        logger.info("try to collect elastic run configs externally")
         if not self._external_config:
             logger.warning("External config not set")
             return {}
