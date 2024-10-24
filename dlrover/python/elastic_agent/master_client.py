829b519e2e11f5a02667955fb34c8a7e671913e9
Tianyi Chen <chentianyi.cty@antfin.com>
2024-10-24 10:43:45 +0800

    add grpc envs (#1305)

--- dlrover/python/elastic_agent/master_client.py
+++ dlrover/python/elastic_agent/master_client.py
@@ -29,7 +29,7 @@ from dlrover.python.diagnosis.common.diagnosis_data import DiagnosisData
 def retry_grpc_request(func):
     def wrapper(self, *args, **kwargs):
         retry = kwargs.get("retry", 10)
-        execption = None
+        exception = None
         for i in range(retry):
             try:
                 return func(self, *args, **kwargs)
@@ -39,13 +39,20 @@ def retry_grpc_request(func):
                 logger.warning(
                     f"Retry {i} to {class_name}.{func_name} with failure",
                 )
-                execption = e
+                exception = e
                 time.sleep(5)
-        if execption:
-            logger.error(execption)
-            raise execption
+        if exception:
+            logger.error(exception)
+            raise exception
 
     return wrapper
+
+
+def init_grpc_env():
+    # At the cost of increased performance overhead, these provide greater
+    # stability in concurrent scenarios.
+    env_utils.set_env("GRPC_ENABLE_FORK_SUPPORT", "true")
+    env_utils.set_env("GRPC_POLL_STRATEGY", "poll")
 
 
 class MasterClient(Singleton):
@@ -75,6 +82,7 @@ class MasterClient(Singleton):
             f"Build master client with master_addr: {master_addr}, "
             f"node_id: {node_id}, node_type: {node_type}."
         )
+        init_grpc_env()
         self._timeout = timeout
         self._master_addr = master_addr
         self._channel = grpc.build_channel(master_addr)
