d994cebd1e2fc078ef78657a6174c4c325d09d53
BO SANG <b.sang@antgroup.com>
2024-11-18 22:41:23 -0800

    fix unit test

--- dlrover/python/master/servicer.py
+++ dlrover/python/master/servicer.py
@@ -99,6 +99,7 @@ class MasterServicer(elastic_training_pb2_grpc.MasterServicer):
         self._diagnosis_data_module = importlib.import_module(
             "dlrover.python.diagnosis.common.diagnosis_data"
         )
+        self._kv_store.clear()
 
     def get(self, request, _):
         node_type = request.node_type
