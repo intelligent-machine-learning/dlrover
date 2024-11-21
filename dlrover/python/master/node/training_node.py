3c108d06ba324f9e4b985a0e85d69c20fdd8a5c4
chentianyi.cty <robert37@sina.com>
2024-11-21 20:16:56 +0800

    fix

--- dlrover/python/master/node/training_node.py
+++ dlrover/python/master/node/training_node.py
@@ -240,7 +240,10 @@ class TrainingNodeManager(object):
 
     def update_nodes_iter(self):
         nodes = self._job_context.job_nodes_by_type(self._node_type)
-        self._node_id_iter = itertools.count(max(nodes.keys()) + 1)
+
+        self._node_id_iter = itertools.count(
+            max(nodes.keys()) + 1 if len(nodes) > 0 else 0
+        )
         self._node_rank_iter = itertools.count(len(nodes))
 
     def remove_node(self, node_id):
