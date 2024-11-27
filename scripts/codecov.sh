8cd2b73b391fc2b6a68094fee118572a44c16e49
mingcheng <mingcheng@apache.org>
2024-11-27 19:07:31 +0800

    open the codcov configure

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#curl -Os https://uploader.codecov.io/latest/linux/codecov
-#chmod +x codecov
-#./codecov
+curl -Os https://uploader.codecov.io/latest/linux/codecov
+chmod +x codecov
+./codecov
