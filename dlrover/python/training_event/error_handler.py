46727368779ce0f479c185816ca45e69b1d1bfa8
liyanzhe.lyz <liyanzhe.lyz@antgroup.com>
2025-01-24 20:55:57 +0800

    fix format

--- dlrover/python/training_event/error_handler.py
+++ dlrover/python/training_event/error_handler.py
@@ -1,15 +1,28 @@
+# Copyright 2025 The DLRover Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import signal
 import sys
 import threading
 import traceback
 
-
-from .config import get_default_logger, _CONFIG
+from .config import get_default_config, get_default_logger
 from .emitter import Process
 from .exporter import close_default_exporter
 
 _LOGGER = get_default_logger()
+_CONFIG = get_default_config()
 
 
 class ErrorHandler:
