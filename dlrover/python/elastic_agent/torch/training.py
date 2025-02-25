b7d86acd42db600892cebb0ad31ca5d2e12eb5f9
chentianyi.cty <robert37@sina.com>
2025-02-25 15:04:57 +0800

    fix

--- dlrover/python/elastic_agent/torch/training.py
+++ dlrover/python/elastic_agent/torch/training.py
@@ -516,7 +516,7 @@ class ElasticTrainingAgent(LocalElasticAgent):
             self._diagnose_agent = DiagnosisAgent.singleton_instance(
                 training_log_file=training_log_file,
                 errors=failure_node_errors,
-                rank=node_rank,
+                node_rank=node_rank,
                 local_world_size=config.nproc_per_node,
             )
         self._agent_context = get_agent_context()
