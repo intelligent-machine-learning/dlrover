7abf8d034ce9bfaf2899ffbb9cce814f5cce7c43
Ma Jie Yue <jieyue.majy@antgroup.com>
2025-04-01 09:43:13 +0800

    add more debuginfo in ci install script

--- scripts/ci_install.sh
+++ scripts/ci_install.sh
@@ -15,11 +15,13 @@
 echo "Start installing CI python packages."
 start_time=$(date +%s)
 
-pip install -q kubernetes
-pip install -q grpcio-tools
-pip install -q psutil
-pip install -q deprecated
-pip install -q tornado
+echo "Upgrading pip tools"
+pip install --upgrade pip
+pip install kubernetes
+pip install grpcio-tools
+pip install psutil
+pip install deprecated
+pip install tornado
 
 if [ "$1" = "basic" ]; then
   echo ""
@@ -29,17 +31,17 @@ if [ "$1" = "basic" ]; then
   exit 0
 fi
 
-pip install -q 'ray[default]'
-pip install -q pyhocon
-pip install -q pytest-cov
-pip install -q pytest-ordering
-pip install -q packaging
-pip install -q tensorflow==2.13.0
-pip install -q torch==2.0.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
-pip install -q deepspeed==0.12.6
-pip install -q accelerate==0.29.2
-pip install -q transformers==4.37.2
-pip install -q peft==0.10.0
+pip install 'ray[default]'
+pip install pyhocon
+pip install pytest-cov
+pip install pytest-ordering
+pip install packaging
+pip install tensorflow==2.13.0
+pip install torch==2.0.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install deepspeed==0.12.6
+pip install accelerate==0.29.2
+pip install transformers==4.37.2
+pip install peft==0.10.0
 
 end_time=$(date +%s)
 cost_time=$((end_time-start_time))
