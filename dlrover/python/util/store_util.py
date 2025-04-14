27d736c5de10d1309f1660974ec9f9423e04bf78
Ma Jie Yue <jieyue.majy@antgroup.com>
2025-04-14 21:18:34 +0800

    add more logs

--- dlrover/python/util/store_util.py
+++ dlrover/python/util/store_util.py
@@ -13,20 +13,26 @@
 
 from datetime import timedelta
 
+from dlrover.python.common.log import default_logger as logger
+
 
 def wait_all(store, rank: int, prefix: str, size: int):
     keys = []
     for idx in range(size):
         key = f"{prefix}{idx}"
         keys.append(key)
+    logger.info(f"Rank{rank} starts waiting for all keys")
     store.wait(keys)
+    logger.info(f"Rank{rank} starts set {prefix}{rank}.FIN")
     store.set(f"{prefix}{rank}.FIN", b"FIN")
     if rank == 0:
         # Rank0 runs the TCPStore daemon, as a result it needs to exit last.
         # Otherwise, the barrier may timeout if rank0 process finished the work
         # before other processes finished `wait_all` method
+        logger.info(f"Rank{rank} starts waiting for FIN flag")
         for node_rank in range(size):
             store.get(f"{prefix}{node_rank}.FIN")
+            logger.info(f"Rank{rank} gets {prefix}{node_rank}.FIN")
 
 
 def synchronize(
@@ -39,6 +45,7 @@ def synchronize(
 ):
     store.set_timeout(timedelta(seconds=barrier_timeout))
     store.set(f"{key_prefix}{rank}", data)
+    logger.info(f"Rank{rank} starts synchronization with size {world_size}")
     wait_all(store, rank, key_prefix, world_size)
 
 
