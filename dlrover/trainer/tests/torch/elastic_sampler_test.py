Fix mismatch for ElasticDistributedSampler when scaling.

dlrover/trainer/tests/torch/elastic_sampler_test.py
@@ -20,8 +20,8 @@ from dlrover.trainer.torch.elastic.sampler import ElasticDistributedSampler
 
 
 class SimpleDataset(Dataset):
-    def __init__(self):
-        self.data = np.arange(0, 60001)
+    def __init__(self, len=60001):
+        self.data = np.arange(0, len)
 
     def __len__(self):
         return len(self.data)
@@ -68,7 +68,7 @@ class ElasticDistributedSamplerTest(unittest.TestCase):
 
         sampler = ElasticDistributedSampler(
             dataset=dataset,
-            num_replicas=3,
+            num_replicas=2,
             rank=0,
             shuffle=False,
             drop_last=True,
@@ -76,3 +76,46 @@ class ElasticDistributedSamplerTest(unittest.TestCase):
         sampler.load_state_dict(sampler_state)
         val = next(iter(sampler))
         self.assertEqual(val, 63)
+
+    def test_checkpoint_with_scaling(self):
+        dataset = SimpleDataset(len=60000)
+        # 1 Train with 8 replicas, epoch is 0
+        batch_size = 8
+        step = 0
+        checkpoint_step = 4
+        sampler = ElasticDistributedSampler(
+            dataset=dataset,
+            num_replicas=8,
+            rank=0,
+            shuffle=False,
+        )
+        sampler.set_epoch(0)
+
+        # 2 Save the checkpoint
+        sampler_state = None
+        for i, v in enumerate(sampler):
+            if i % batch_size == 0:
+                step = i / batch_size
+            if step == checkpoint_step:
+                sampler_state = sampler.state_dict(step, batch_size)
+                break
+        self.assertEqual(
+            sampler_state["completed_num"], 8 * batch_size * checkpoint_step
+        )
+
+        # 3 Resume with 6 replicas from checkpoint, and epoch is 0
+        sampler.set_epoch(0)
+        sampler = ElasticDistributedSampler(
+            dataset=dataset,
+            num_replicas=6,
+            rank=0,
+            shuffle=False,
+        )
+        sampler.load_state_dict(sampler_state)
+        for i in sampler:
+            pass
+
+        # 4 Continue, but epoch is 1
+        sampler.set_epoch(1)
+        for i in sampler:
+            pass