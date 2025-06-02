fecf066856a58b2b3551b57a9fa96065199d08ac
youxingling <youxingling1@gmail.com>
2025-06-02 14:21:04 +0000

    Get master_handler and close.

--- dlrover/trainer/torch/elastic_run.py
+++ dlrover/trainer/torch/elastic_run.py
@@ -514,7 +514,7 @@ def _check_to_use_dlrover_run(job_name, is_standalone=False):
                     return False
                 else:
                     # dlrover-run + local-master(standalone)
-                    return True
+                    return True, master_handler
             else:
                 # raise exception directly
                 raise RuntimeError(
@@ -552,7 +552,7 @@ def run(args):
 
     is_standalone = args.standalone
     logger.info(f"Standalone mode: {is_standalone}")
-    use_dlrover_launch = _check_to_use_dlrover_run(job_name, is_standalone)
+    use_dlrover_launch, master_handler = _check_to_use_dlrover_run(job_name, is_standalone)
 
     # for torchrun standalone mode
     if is_standalone and not use_dlrover_launch:
