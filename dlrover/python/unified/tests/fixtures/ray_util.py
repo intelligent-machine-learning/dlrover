565d97486148ab9c09bc9ecbb3e769c1050b5902
WayZer <q10482037873@gmail.com>
2025-07-04 16:14:19 +0800

    lint

--- dlrover/python/unified/tests/fixtures/ray_util.py
+++ dlrover/python/unified/tests/fixtures/ray_util.py
@@ -1,3 +1,16 @@
+# Copyright 2025 The DLRover Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from contextlib import contextmanager
 from pathlib import Path
 from unittest.mock import patch
@@ -13,9 +26,9 @@ from dlrover.python.unified.util.test_hooks import coverage_enabled
 def disable_ray_auto_init():
     def auto_init_ray():
         """Patch to disable ray auto init in tests."""
-        assert ray.is_initialized(), (
-            "Ray should be initialized before using Ray APIs."
-        )
+        assert (
+            ray.is_initialized()
+        ), "Ray should be initialized before using Ray APIs."
 
     with patch("ray._private.auto_init_hook.auto_init_ray", auto_init_ray):
         yield
