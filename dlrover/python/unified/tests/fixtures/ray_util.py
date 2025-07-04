794af17d1d45bbd6a2b1b5d8422aedf19cd1bc8e
WayZer <q10482037873@gmail.com>
2025-07-04 11:07:00 +0800

    fix ut: disable ray auto init

--- dlrover/python/unified/tests/fixtures/ray_util.py
+++ dlrover/python/unified/tests/fixtures/ray_util.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 import ray
@@ -8,16 +9,40 @@ from dlrover.python.unified.util.actor_helper import kill_actors
 from dlrover.python.unified.util.test_hooks import coverage_enabled
 
 
-@contextmanager
-def _setup_ray():
-    """Setup coverage environment variables if coverage is enabled."""
+@pytest.fixture(scope="session", autouse=True)
+def disable_ray_auto_init():
+    def auto_init_ray():
+        """Patch to disable ray auto init in tests."""
+        assert (
+            ray.is_initialized()
+        ), "Ray should be initialized before using Ray APIs."
+
+    with patch("ray._private.auto_init_hook.auto_init_ray", auto_init_ray):
+        yield
+
+
+@pytest.fixture(scope="session")
+def coverage_envs():
+    """Fixture to set up coverage environment variables."""
     envs = {}
     if coverage_enabled():
         file = Path(__file__).parent / "ray.coveragerc"
         envs["COVERAGE_PROCESS_START"] = file.as_posix()
         print("Coverage enabled, setting up environment variables for ray.")
         print(f"  COVERAGE_PROCESS_START={file.as_posix()}")
+    yield envs
+    if coverage_enabled():
+        print("Combining coverage data...")
+        import coverage
 
+        coverage.Coverage().combine()
+
+
+@contextmanager
+def _setup_ray(envs):
+    """Setup coverage environment variables if coverage is enabled."""
+    if ray.is_initialized():
+        pytest.fail("Ray is already initialized before setup.")
     ray.init(
         namespace="dlrover_test",
         runtime_env={"env_vars": envs},
@@ -29,23 +54,18 @@ def _setup_ray():
         print(f"Cleaning up {len(actors)} actors...: {actors}")
         kill_actors(actors)
     ray.shutdown()
-    if coverage_enabled():
-        print("Combining coverage data...")
-        import coverage
-
-        coverage.Coverage().combine()
 
 
 @pytest.fixture()
-def tmp_ray():
+def tmp_ray(coverage_envs):
     """Fixture to initialize and shutdown Ray."""
-    with _setup_ray():
+    with _setup_ray(coverage_envs):
         yield
 
 
-@pytest.fixture(scope="session")
-def session_ray():
-    """Fixture to initialize and shutdown Ray.
-    Session-scoped, shared across tests."""
-    with _setup_ray():
+@pytest.fixture(scope="module")
+def shared_ray(coverage_envs):
+    """Fixture to initialize and shutdown Ray. Shared across tests.
+    Module scope to avoid affecting other modules."""
+    with _setup_ray(coverage_envs):
         yield
