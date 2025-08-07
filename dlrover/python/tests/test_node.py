387894c8a9236a4254bbc97b00d57194944254f0
chentianyi.cty <robert37@sina.com>
2025-08-07 16:53:13 +0800

    refactor relaunch node generation

--- dlrover/python/tests/test_node.py
+++ dlrover/python/tests/test_node.py
@@ -10,16 +10,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import time
 import unittest
 
+from dlrover.python.common.comm import ParallelConfig
 from dlrover.python.common.constants import (
     NodeEventType,
     NodeExitReason,
     NodeResourceLimit,
     NodeStatus,
+    NodeType,
 )
-from dlrover.python.common.node import Node, NodeEvent
+from dlrover.python.common.node import Node, NodeEvent, NodeResource
 
 
 class NodeTest(unittest.TestCase):
@@ -92,7 +94,7 @@ class NodeTest(unittest.TestCase):
         node.id = 100
         node.update_from_node(node)
 
-        node = node.get_relaunch_node_info(123)
+        node = node.generate_relaunch_node(123)
         self.assertEqual(node.id, 123)
         self.assertFalse(node.reported_status[0])
 
@@ -120,3 +122,68 @@ class NodeTest(unittest.TestCase):
         node.name = "node"
         name = node.get_name()
         self.assertEqual(name, "node")
+
+    def test_generate_relaunch_node(self):
+        node = Node(
+            NodeType.WORKER,
+            1,
+            NodeResource(2, 2000),
+            name="test-1",
+            status=NodeStatus.RUNNING,
+            start_time=time.time(),
+            rank_index=1,
+            relaunch_count=2,
+            critical=False,
+            max_relaunch_count=5,
+            relaunchable=True,
+            service_addr="test-1.service",
+            host_name="test-1.host",
+            host_ip="1.1.1.1",
+            paral_config=ParallelConfig(),
+            restart_training=True,
+            node_group=1,
+            node_group_size=8,
+            node_group_id=3,
+        )
+        node.init_time = time.time()
+        node.start_hang_time = time.time()
+        node.finish_time = time.time()
+        node.heartbeat_time = time.time()
+        node.hang = True
+        node.is_released = True
+
+        relaunch_node = node.generate_relaunch_node(2, "test-2")
+        self.assertNotEqual(node.id, relaunch_node.id)
+        self.assertNotEqual(node.name, relaunch_node.name)
+        self.assertNotEqual(
+            node.config_resource, relaunch_node.config_resource
+        )
+        self.assertNotEqual(node.init_time, relaunch_node.init_time)
+        self.assertNotEqual(node.start_time, relaunch_node.start_time)
+        self.assertNotEqual(node.finish_time, relaunch_node.finish_time)
+        self.assertNotEqual(node.heartbeat_time, relaunch_node.heartbeat_time)
+        self.assertNotEqual(
+            node.start_hang_time, relaunch_node.start_hang_time
+        )
+        self.assertNotEqual(node.status, relaunch_node.status)
+        self.assertNotEqual(node.is_released, relaunch_node.is_released)
+        self.assertNotEqual(node.host_ip, relaunch_node.host_ip)
+        self.assertNotEqual(node.host_name, relaunch_node.host_name)
+
+        self.assertEqual(
+            node.config_resource.to_resource_dict(),
+            relaunch_node.config_resource.to_resource_dict(),
+        )
+        self.assertEqual(node.type, relaunch_node.type)
+        self.assertEqual(node.rank_index, relaunch_node.rank_index)
+        self.assertEqual(node.relaunch_count, relaunch_node.relaunch_count)
+        self.assertEqual(node.critical, relaunch_node.critical)
+        self.assertEqual(
+            node.max_relaunch_count, relaunch_node.max_relaunch_count
+        )
+        self.assertEqual(node.relaunchable, relaunch_node.relaunchable)
+        self.assertEqual(node.service_addr, relaunch_node.service_addr)
+        self.assertEqual(node.paral_config, relaunch_node.paral_config)
+        self.assertEqual(node.group, relaunch_node.group)
+        self.assertEqual(node.group_id, relaunch_node.group_id)
+        self.assertEqual(node.group_size, relaunch_node.group_size)
