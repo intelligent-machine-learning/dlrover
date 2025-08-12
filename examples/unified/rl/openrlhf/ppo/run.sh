b892e9cab4f025ad4f5b6ecde8cee1bc0be543f7
WayZer <q10482037873@gmail.com>
2025-08-12 15:15:29 +0800

    lint

--- examples/unified/rl/openrlhf/ppo/run.sh
+++ examples/unified/rl/openrlhf/ppo/run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 # Copyright 2025 The DLRover Authors. All rights reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
