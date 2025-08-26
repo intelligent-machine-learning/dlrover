55417c55e14cb9ad7faf94727f04dc99d37bc5c3
WayZer <q10482037873@gmail.com>
2025-08-26 11:17:58 +0800

    add ut for _get_ray_gpu_devices

--- dlrover/python/unified/tests/backend/test_elastic.py
+++ dlrover/python/unified/tests/backend/test_elastic.py
@@ -11,11 +11,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from typing import List
 from unittest.mock import Mock
 
+import pytest
+
 from dlrover.python.unified.backend.elastic.node_check_manager import (
     NodeCheckManager,
+)
+from dlrover.python.unified.backend.elastic.worker.worker import (
+    _get_ray_gpu_devices,
 )
 from dlrover.python.unified.common.workload_base import ActorInfo
 
@@ -35,3 +41,22 @@ def test_find_straggling_nodes():
     assert len(res) == 2, "Two straggling nodes expected"
     assert res[0] == nodes[1], "Expected node at index 1 to be straggling"
     assert res[1] == nodes[3], "Expected node at index 3 to be straggling"
+
+
+def test_get_ray_gpu_devices(mocker):
+    mocker.patch("ray.get_gpu_ids", return_value=[])
+    os.environ.pop("CUDA_VISIBLE_DEVICES", None)
+    assert [it.type for it in _get_ray_gpu_devices()] == ["cpu"]
+
+    mocker.patch("ray.get_gpu_ids", return_value=[0, 1])
+    os.environ.pop("CUDA_VISIBLE_DEVICES", None)
+    assert [it.index for it in _get_ray_gpu_devices()] == [0, 1]
+
+    mocker.patch("ray.get_gpu_ids", return_value=[7, 8])
+    os.environ["CUDA_VISIBLE_DEVICES"] = "5,6,7,8"
+    assert [it.index for it in _get_ray_gpu_devices()] == [2, 3]
+
+    with pytest.raises(RuntimeError):
+        mocker.patch("ray.get_gpu_ids", return_value=[7, 8])
+        os.environ["CUDA_VISIBLE_DEVICES"] = "5,6"
+        _ = _get_ray_gpu_devices()
