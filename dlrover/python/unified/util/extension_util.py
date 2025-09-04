e237d262b526d7a6c8aaf45b5e1941a6a35963f8
WayZer <q10482037873@gmail.com>
2025-09-04 14:49:59 +0800

    fix detect_mixin_conflicts

--- dlrover/python/unified/util/extension_util.py
+++ dlrover/python/unified/util/extension_util.py
@@ -1,5 +1,18 @@
+# Copyright 2025 The DLRover Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from collections import defaultdict
-from typing import ClassVar, List
+from typing import ClassVar, Dict, List
 
 from dlrover.python.common.log import default_logger as logger
 from dlrover.python.unified.util.test_hooks import after_test_cleanup
@@ -32,9 +45,9 @@ class Extensible:
         )
 
         try:
-            return type(
-                f"Mixed_{cls.__name__}", tuple(extensions) + (cls,), {}
-            )
+            ret = type(f"Mixed_{cls.__name__}", tuple(extensions) + (cls,), {})
+            logger.info(f"{ret.__name__} built with {ret.__mro__}")
+            return ret
         except Exception:
             logger.exception(f"Failed to create mixed class for {cls}")
             detect_mixin_conflicts(extensions, cls)
@@ -56,14 +69,14 @@ def get_overridden_methods(mixin: type, base: type) -> set:
 
 
 def detect_mixin_conflicts(mixins: List[type], base: type):
-    method_map = defaultdict(list)
+    method_map: Dict[str, List[type]] = defaultdict(list)
     for mixin in mixins:
-        impl = []
         for method in get_overridden_methods(mixin, base):
-            impl.append(mixin.__name__)
+            method_map[method].append(mixin)
+    for method, impl in method_map.items():
         if len(impl) > 1:
             logger.warning(
-                f"Mixin {mixin.__name__} overrides multiple methods: {impl}"
+                f"Method {method} is overridden by multiple mixins: {impl}"
             )
 
     return {k: v for k, v in method_map.items() if len(v) > 1}
