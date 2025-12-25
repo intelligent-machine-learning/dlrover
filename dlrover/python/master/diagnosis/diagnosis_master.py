9fee4b5c2b7ab125db57d19288b25dffe9b6dfaf
BO SANG <b.sang@antgroup.com>
2025-12-24 16:21:20 -0800

    refactor diagnosis master

--- dlrover/python/master/diagnosis/diagnosis_master.py
+++ dlrover/python/master/diagnosis/diagnosis_master.py
@@ -40,20 +40,8 @@ from dlrover.python.diagnosis.common.constants import (
 from dlrover.python.diagnosis.common.diagnosis_action import NodeAction
 from dlrover.python.diagnosis.common.diagnosis_data import DiagnosisData
 from dlrover.python.diagnosis.common.diagnosis_manager import DiagnosisManager
-from dlrover.python.diagnosis.common.inference_chain import (
-    InferenceAttribute,
-    InferenceDescription,
-    InferenceName,
-)
 from dlrover.python.diagnosis.diagnostician.node_inconsistency import (
     NodeInconsistencyDiagnostician,
-)
-from dlrover.python.diagnosis.inferencechain.inference_chain import Inference
-from dlrover.python.diagnosis.inferencechain.inferenceoperator.observer.check_training_hang_operator import (  # noqa: E501
-    CheckTrainingHangOperator,
-)
-from dlrover.python.diagnosis.inferencechain.inferenceoperator.resolver.resolve_training_hang_operator import (  # noqa: E501
-    ResolveTrainingHangOperator,
 )
 from dlrover.python.master.diagnosis.diagnosis import Diagnostician
 from dlrover.python.master.diagnosis.diagnosis_data_manager import (
@@ -325,36 +313,8 @@ class DiagnosisMaster(DiagnosisManager):
             NodeInconsistencyDiagnostician(self._job_args),
             60 * 5,
         )
-
-        # TODO: need refactor
-        self._diagnostician.register_training_problems(
-            [
-                Inference(
-                    InferenceName.TRAINING,
-                    InferenceAttribute.ISORNOT,
-                    InferenceDescription.HANG,
-                )
-            ]
-        )
-        self._diagnostician.register_observers(
-            [CheckTrainingHangOperator(self._data_manager)]
-        )
-        self._diagnostician.register_resolvers(
-            [ResolveTrainingHangOperator(self._data_manager)]
-        )
-
         try:
             self.start_diagnosis()
-
-            # TODO: need refactor
-            diag = threading.Thread(
-                target=self._diagnose,
-                name="diagnose_failures",
-                daemon=True,
-            )
-            diag.start()
-            if diag.is_alive():
-                logger.info("_diagnose thread has started")
 
             diag_metric = threading.Thread(
                 target=self._diagnose_metrics,
@@ -479,20 +439,3 @@ class DiagnosisMaster(DiagnosisManager):
                             )
 
             time.sleep(interval)
-
-    def _diagnose(self):
-        logger.info("_diagnose thread is running...")
-        while True:
-            if not self._is_observing_started:
-                logger.info(
-                    f"Stop _diagnose thread due to {self._is_observing_started}"
-                )
-                break
-
-            observed_problems = self._diagnostician.observe_training()
-            action = self._diagnostician.resolve_problems(observed_problems)
-            self._job_context.enqueue_diagnosis_action(action)
-
-            time.sleep(
-                DiagnosisConstant.MASTER_DIAGNOSIS_OBSERVING_INTERVAL_SECS
-            )
