75644f4629bd9ab86ac1b5711b4d21117551dce4
Tianyi Chen <chentianyi.cty@antfin.com>
2026-01-06 10:44:37 +0800

    Update setup.py to 0.6.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ extra_require = {
 
 setup(
     name="dlrover",
-    version="0.7.0.dev0",
+    version="0.6.1",
     description="An Automatic Distributed Deep Learning Framework",
     long_description="DLRover helps model developers focus on model algorithm"
     " itself, without taking care of any engineering stuff,"
